201be045d93abe3d5d7675fc6b2e0929a96408bc
Zou Zhefeng <archer.zou84@gmail.com>
2026-03-11 00:51:37 +1300

    Add project files.

---
+++ Movies.Contracts/Responses/MoviesResponse.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Movies.Contracts.Responses;
+
+public class MoviesResponse
+{
+    public required IEnumerable<MovieResponse> Items { get; init; } = Enumerable.Empty<MovieResponse>();
+}
